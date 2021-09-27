734850282bd90ea15da50fe293d2307af2447f4a
noam <noamsi@post.bgu.ac.il>
2021-09-28 01:21:54 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     author_email='noamsi@post.bgu.ac.il',
     description='school stuff',
     packages=find_packages(),    
-    install_requires=['numpy >= 1.16.1', 'matplotlib >= 3.3.4', 'mne >= 0.23.4'],
+    install_requires=['numpy >= 1.16.1', 'matplotlib >= 3.3.4', 'mne >= 0.23.4',
+                      'seaborn >= 0.11.1'],
 )
