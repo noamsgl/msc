7675367805195f96454fa87f776a668016c19995
noam <noamsi@post.bgu.ac.il>
2021-11-02 10:47:27 +0200

    add simple GP regression with EEG

--- msc/etl/load.py
+++ msc/etl/load.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 import mne
 import os
 
+import numpy as np
 import torch
 from mne.io import Raw
 
@@ -14,9 +15,15 @@ __ALL_CHANNELS__ = ('FP1', 'FP2', 'F3', 'F4', 'C3', 'C4', 'P3', 'P4', 'O1', 'O2'
 __COMMON_CHANNELS__ = (
     'FP1', 'FP2', 'F3', 'F4', 'C3', 'C4', 'P3', 'P4', 'O1', 'O2', 'F7', 'F8', 'T3', 'T4', 'T5', 'T6', 'FZ', 'CZ', 'PZ',
     'T7', 'T8', 'P7', 'P8')
+
 __FPATH__ = r'C:\temp\surf30\pat_103002\adm_1030102\rec_103001102\103001102_0113.data'
 
-__CROP__ = 10
+__LFREQ__ = 0.5  # low frequency for highpass filter
+__HFREQ__ = 5  # high frequency for lowpass filter
+__CROP__ = 10  # segment length measured in seconds
+__TRAIN_LENGTH__ = 10  # segment length measured in seconds
+__TEST_LENGTH__ = 2  # segment length measured in seconds
+
 
 @dataclass
 class PicksOptions:
@@ -46,10 +53,11 @@ def load_raw_data(fpath: str = __FPATH__, crop: int = __CROP__, picks: Tuple[str
     if picks:
         print(f"picks: {picks}")
     extension = os.path.splitext(fpath)[1]
-    preload = False
+    preload = True
     if extension == '.data':
         # file is stored in raw nicolet format
-        raw: Raw = mne.io.read_raw_nicolet(fpath, ch_type='eeg', preload=preload).pick(picks).crop(0, crop)
+        raw: Raw = mne.io.read_raw_nicolet(fpath, ch_type='eeg', preload=preload).pick(picks).crop(0, crop).filter(
+            l_freq=__LFREQ__, h_freq=__HFREQ__)
     elif extension == ".edf":
         raw = mne.io.read_raw_edf(fpath, picks=picks, preload=preload).crop(0, crop)
     else:
@@ -57,9 +65,16 @@ def load_raw_data(fpath: str = __FPATH__, crop: int = __CROP__, picks: Tuple[str
     return raw
 
 
-def load_tensor_dataset(fpath: str = __FPATH__, crop: int = __CROP__, picks: Tuple[str] = __PICKS__):
-    raw: Raw = load_raw_data(fpath, crop, picks)
-    data, times, = raw.get_data(return_times=True)
-    dataset = {"train_x": torch.Tensor(times),
-               "train_y": torch.Tensor(data).T}
+def load_tensor_dataset(fpath: str = __FPATH__, train_length: int = __TRAIN_LENGTH__,
+                        test_length: int = __TEST_LENGTH__, picks: Tuple[str] = __PICKS__):
+    raw: Raw = load_raw_data(fpath, __TRAIN_LENGTH__ + __TEST_LENGTH__, picks)
+    data, times = raw.get_data(return_times=True)
+
+    split_index = np.argmax(times > __TRAIN_LENGTH__)
+
+    dataset = {"train_x": torch.Tensor(times[:split_index]),
+               "train_y": torch.Tensor(data[:, :split_index]).T.squeeze(),
+               "test_x": torch.Tensor(times[split_index:]),
+               "test_y": torch.Tensor(data[:, split_index:]).T.squeeze()
+               }
     return dataset
