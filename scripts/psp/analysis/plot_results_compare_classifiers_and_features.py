ac686ef2bcd78977e58f670eb69c73a3eb1ac0e4
noam <noamsi@post.bgu.ac.il>
2022-01-10 16:29:40 +0200

    add plot_results_compare_classifiers_and_features.py

--- scripts/psp/analysis/plot_results_compare_classifiers_and_features.py
+++ scripts/psp/analysis/plot_results_compare_classifiers_and_features.py
@@ -1,5 +1,7 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 from msc.config import get_config
 
@@ -50,10 +52,42 @@ if __name__ == '__main__':
     config = get_config()
     results_fpath = f"{config['PATH']['LOCAL']['RESULTS']}/complete_results.csv"
     results = pd.read_csv(results_fpath)
+    selected_features = ['max_cross_corr', 'phase_lock_val', 'spect_corr', 'time_corr']
+    results = results.loc[results.feature_name.apply(lambda val: val in selected_features)]
+    # create plot
+    # ax = plt.axes()
+    # ax.set_title(f'ROC AUC for patient {pat_number}')
+    # sns.heatmap(frame, annot=True, ax=ax)
+    # ax.figure.tight_layout()
+    # plt.show()
+    pat_numbers = [3500, 3700, 7200]
 
-    selected_patient = "pat_7200"
+    matplotlib.rc('xtick', labelsize=16)
+    matplotlib.rc('ytick', labelsize=16)
 
+    fig, axes = plt.subplots(1, 3, figsize=(13.33, 7.5), dpi=180, sharex=True, sharey=True)
+    cbar_ax = fig.add_axes([.91, .28, .03, .6])
+    fig.suptitle("Training Time", fontsize=21)
+    # fig.text(0.5, 0.04, 'Feature', ha='center')
+    # fig.text(0.04, 0.5, 'Classifier', va='center', rotation='vertical')
+    for i, ax in enumerate(axes.flat):
+        pat_number = pat_numbers[i]
 
+        selected_patient = f"pat_{pat_number}"
+
+        # build results frame
+        frame = results.set_index('patient_name').loc[selected_patient].groupby(
+            ['classifier_name', 'feature_name']).aggregate('mean').test_roc_auc.unstack(0)
+        ax.set_title(f'patient {pat_number}', fontsize=16)
+        sns.heatmap(frame.transpose(),
+                    cmap=sns.color_palette('Spectral', as_cmap=True),
+                    annot=True, ax=ax, cbar=i == 0,
+                    vmin=0, vmax=1, cbar_ax=None if i else cbar_ax, annot_kws={"fontsize": 12}, )
+        ax.set_ylabel('')
+        ax.set_xlabel('')
+        ax.figure.tight_layout()
+    fig.tight_layout(rect=[0, 0, .9, 1])
+    plt.show()
     # selected_feature = "spect_corr"
     #
     # results = results.set_index(['patient_name', 'feature_name']).loc[selected_patient, selected_feature]
