d4f4896f939b8ceb226f2bcd7703b55ee2a533d9
noam <noamsi@post.bgu.ac.il>
2022-01-19 11:22:49 +0200

    integrate clearML

--- scripts/inference/gp_canine_db_mle.py
+++ scripts/inference/gp_canine_db_mle.py
@@ -4,9 +4,11 @@ import pytorch_lightning as pl
 import torch
 from clearml import Task
 from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
 from torch import Tensor
 from torch.utils.data import DataLoader
 
+from msc import config
 from msc.dataset import DogDataset, SingleSampleDataset
 
 
@@ -58,7 +60,6 @@ class SingleSampleEEGGPModel(pl.LightningModule):
         raise NotImplementedError()
 
 
-
 if __name__ == '__main__':
     seed_everything(42)
     n_draws = 8
@@ -67,15 +68,19 @@ if __name__ == '__main__':
     samples_df = dataset.samples_df
 
     selected_fname = 'Dog_1_ictal_segment_2.mat'
+
     selected_ch_names = ['NVC0905_22_002_Ecog_c001', 'NVC0905_22_002_Ecog_c002',
-       'NVC0905_22_002_Ecog_c003', 'NVC0905_22_002_Ecog_c004',
-       'NVC0905_22_002_Ecog_c005', 'NVC0905_22_002_Ecog_c006',
-       'NVC0905_22_002_Ecog_c007', 'NVC0905_22_002_Ecog_c008',
-       'NVC0905_22_002_Ecog_c009', 'NVC0905_22_002_Ecog_c010',
-       'NVC0905_22_002_Ecog_c011', 'NVC0905_22_002_Ecog_c012',
-       'NVC0905_22_002_Ecog_c013', 'NVC0905_22_002_Ecog_c014',
-       'NVC0905_22_002_Ecog_c015', 'NVC0905_22_002_Ecog_c016']
-    samples_df = samples_df.loc[samples_df['fname'] == selected_fname, selected_ch_names + ['time', 'fname']]
+                         'NVC0905_22_002_Ecog_c003', 'NVC0905_22_002_Ecog_c004',
+                         'NVC0905_22_002_Ecog_c005', 'NVC0905_22_002_Ecog_c006',
+                         'NVC0905_22_002_Ecog_c007', 'NVC0905_22_002_Ecog_c008',
+                         'NVC0905_22_002_Ecog_c009', 'NVC0905_22_002_Ecog_c010',
+                         'NVC0905_22_002_Ecog_c011', 'NVC0905_22_002_Ecog_c012',
+                         'NVC0905_22_002_Ecog_c013', 'NVC0905_22_002_Ecog_c014',
+                         'NVC0905_22_002_Ecog_c015', 'NVC0905_22_002_Ecog_c016'
+                         ]
+    samples_df = samples_df.loc[:,
+                 # samples_df['fname'] == selected_fname,
+                 selected_ch_names + ['time', 'fname']]
 
     for fname, group in samples_df.groupby('fname'):
         for ch_name in selected_ch_names:
@@ -83,7 +88,7 @@ if __name__ == '__main__':
             task = Task.init(project_name=f"inference/{fname}", task_name=f"gp_{ch_name}")
             # override parameters with provided dictionary
             hparams = {'learning_rate': 1e-2,
-                       'n_epochs': 400}
+                       'n_epochs': 1000}
             task.set_parameters(hparams)
 
             train_x = Tensor(group['time'].values)
@@ -94,7 +99,6 @@ if __name__ == '__main__':
             plt.title('Data Sample')
             plt.show()
 
-
             model = SingleSampleEEGGPModel(hparams, train_x, train_y)
 
             for i in range(n_draws):
@@ -103,9 +107,20 @@ if __name__ == '__main__':
             plt.title('Prior')
             plt.show()
 
-            trainer = Trainer(max_epochs=hparams['n_epochs'], log_every_n_steps=1, gpus=1, profiler=False, enable_checkpointing=False)
+            # saves a file like: my/path/sample-mnist-epoch=02-val_loss=0.32.ckpt
+            checkpoint_callback = ModelCheckpoint(
+                monitor="train_loss",
+                dirpath=f"{config['PATH']['LOCAL']['LIGHTNING_LOGS']}/{fname[:-4]}/{ch_name[-4:]}",
+                filename="gp_inference-{epoch:03d}-{train_loss:.2f}",
+                save_top_k=1,
+                mode="min",
+                every_n_epochs=1000
+            )
+
+            trainer = Trainer(max_epochs=hparams['n_epochs'], log_every_n_steps=1, gpus=1, profiler=False,
+                              callbacks=[checkpoint_callback], fast_dev_run=False)
             trainer.fit(model, train_dataloader)
-            trainer.save_checkpoint('trained_model.ckpt')
+            # trainer.save_checkpoint('trained_model.ckpt')
 
             for i in range(n_draws):
                 plt.plot(train_x, model(train_x).sample())
