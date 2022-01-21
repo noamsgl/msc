4df9dfd96681a356bff5392a8decabad26ee961a
noam <noamsi@post.bgu.ac.il>
2022-01-21 13:31:01 +0200

    finish gp_analyze_inference.py clearML

--- scripts/inference/gp_analyze_inference.py
+++ scripts/inference/gp_analyze_inference.py
@@ -9,18 +9,13 @@ Gaussian Process Analyze Inference
 
 
 """
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from clearml import Task
 from clearml.backend_api import Session
+from clearml.backend_api.services import tasks, projects
 from requests import Request
-from clearml.backend_api.services import tasks, events, projects
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from msc.dataset import DogDataset
-from msc.models import SingleSampleEEGGPModel
-
-
 
 
 class GPResultsCollector:
@@ -30,25 +25,32 @@ class GPResultsCollector:
 
         # get projects matching the name 'inference/'
         req = Request('POST')
-        res = session.send(projects.GetAllRequest(name='inference'))
+        projects_res = session.send(projects.GetAllRequest(name='inference'))
         # get all the project Ids matching the project name 'inference'
-        project_ids = [p.id for p in res.response.projects]
+        project_ids = [p.id for p in projects_res.response.projects]
+
+        projects_df = pd.DataFrame([p.to_dict() for p in projects_res.response.projects])
+        projects_df["label_desc"] = projects_df["name"].apply(
+            lambda name: 'interictal' if 'interictal' in name else 'ictal')
 
         # get all the tasks
         tasks_list = []
         for i in range(5):
             print(f"getting page={i}")
-            res = session.send(tasks.GetAllRequest(project=project_ids, page_size=500, page=i))
-            tasks_list += res.response_data['tasks']
+            tasks_res = session.send(tasks.GetAllRequest(project=project_ids, page_size=500, page=i))
+            tasks_list += tasks_res.response_data['tasks']
 
         results_df = pd.DataFrame(tasks_list)
         results_df = self.parse_last_metrics(results_df, requested_params)
 
-        projs_dfs = []
+        results_df = pd.merge(results_df, projects_df[['id', 'name', 'label_desc']], left_on='project', right_on='id',
+                              suffixes=('_task', '_project'))
+        self.results_df = results_df.dropna(subset=requested_params)
 
     @staticmethod
     def parse_last_metrics(results_df, requested_params):
         """last_metrics is a field returned by the ClearML API. This function parses the field results and appends requested_params values to results_df"""
+
         def get_param_from_last_metrics(last_metrics: dict, param_name):
             for value in last_metrics.values():
                 assert isinstance(value, dict), "Error: last_metrics value entries should be of type dict"
@@ -61,15 +63,23 @@ class GPResultsCollector:
             return None
 
         for param_name in requested_params:
-            results_df[param_name] = results_df["last_metrics"].apply(get_param_from_last_metrics, param_name=param_name)
+            results_df[param_name] = results_df["last_metrics"].apply(get_param_from_last_metrics,
+                                                                      param_name=param_name)
 
         return results_df
 
 
 if __name__ == '__main__':
+    task = Task.init(project_name=f"analyze", task_name=f"gp_matern_params")
     requested_params = ['covar_module.raw_outputscale', 'covar_module.base_kernel.raw_lengthscale']
-    results_df = GPResultsCollector(requested_params)
+
+    results_df = GPResultsCollector(requested_params).results_df
+
+    logger = task.get_logger()
+    logger.report_table("results_df", "Results Table", table_plot=results_df)
+    logger.report_scalar('num_samples', 'num_samples', len(results_df), iteration=0)
     sns.jointplot(data=results_df,
                   x=requested_params[0], y=requested_params[1],
                   hue="label_desc", palette="muted")
+    plt.suptitle("Matern Kernel Params for Dog_1 Dataset")
     plt.show()
