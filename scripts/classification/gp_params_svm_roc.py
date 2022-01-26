420f0ff0082c4a8137be2a8a711746f03f772bf1
noam <noamsi@post.bgu.ac.il>
2022-01-26 17:31:56 +0200

    improve plots

--- scripts/classification/gp_params_svm_roc.py
+++ scripts/classification/gp_params_svm_roc.py
@@ -5,7 +5,7 @@ from clearml import Task
 from mlxtend.plotting import plot_decision_regions
 from pytorch_lightning import seed_everything
 import matplotlib.pyplot as plt
-from sklearn import svm
+from sklearn import svm, preprocessing
 from sklearn.model_selection import train_test_split
 
 from msc.results_collectors import GPResultsCollector
@@ -13,8 +13,8 @@ from msc.results_collectors import GPResultsCollector
 if __name__ == '__main__':
     seed_everything(42)
     task = Task.init(project_name="density_estimation", task_name="gp_matern_params_svm", reuse_last_task_id=True)
-    hparams = {'num_samples': 16}
-    task.set_parameters(hparams)
+    # hparams = {'num_samples': 16}
+    # task.set_parameters(hparams)
 
 
     requested_params = ['covar_module.raw_outputscale', 'covar_module.base_kernel.raw_lengthscale']
@@ -35,9 +35,14 @@ if __name__ == '__main__':
     plt.suptitle("Matern Kernel Params for Dog_1 Dataset")
     plt.show()
 
+    # encode labels
+    le = preprocessing.LabelEncoder()
+    le.fit(results_df.label_desc)
+    results_df["label"] = le.transform(results_df.label_desc)
+
     # get all data
     X = results_df[requested_params].to_numpy()
-    y = results_df["label_desc"].to_numpy()
+    y = results_df["label"].to_numpy()
 
     # split train, test
     X_train, X_test, y_train, y_test = train_test_split(X, y)
@@ -56,6 +61,6 @@ if __name__ == '__main__':
     plt.show()
 
     # plot ROC curve
-    sklearn.metric.RocCurveDisplay.from_estimator(clf, X_test, y_test)
+    sklearn.metrics.RocCurveDisplay.from_estimator(clf, X_test, y_test)
     plt.title("ROC curve for SVM on Dog 1 GP Matern Params")
     plt.show()
