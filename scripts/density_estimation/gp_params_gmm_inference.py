60bff550f7b2345f1752af77ec3da7cbacfd63c7
noam <noamsi@post.bgu.ac.il>
2022-01-30 22:30:02 +0200

    refactor: rename

--- scripts/density_estimation/gp_params_gmm_inference.py
+++ scripts/density_estimation/gp_params_gmm_inference.py
@@ -9,11 +9,14 @@ import pandas as pd
 import seaborn as sns
 import torch
 from clearml import Task
+from joblib import dump
 from matplotlib.colors import LogNorm
 from pytorch_lightning import seed_everything
 from sklearn import mixture
 from sklearn.preprocessing import StandardScaler
 
+from msc import config
+from msc.data_utils import get_time_as_str
 from msc.results_collectors import GPResultsCollector
 
 
@@ -28,7 +31,8 @@ if __name__ == '__main__':
     seed_everything(42)
     task = Task.init(project_name="density_estimation", task_name="gp_matern_params_gmm_sklearn",
                      reuse_last_task_id=True)
-    hparams = {'num_samples': 50}
+    hparams = {'num_samples': 50,
+               'selected_label_desc': "ictal"}
     task.set_parameters(hparams)
 
     requested_params = ['covar_module.raw_outputscale', 'covar_module.base_kernel.raw_lengthscale']
@@ -41,15 +45,18 @@ if __name__ == '__main__':
         results_fpath = r"C:\Users\noam\Repositories\noamsgl\msc\results\params\results_gp_dog1_params.csv"
         results_df = pd.read_csv(results_fpath)
 
-    # plot data join plot (before inference)
-    sns.jointplot(data=results_df,
+    # plot data join plot (selected label desc)
+    hue_order = ["ictal", "interictal"]
+    sns.jointplot(data=results_df.loc[results_df["label_desc"] == hparams['selected_label_desc']],
                   x=requested_params[0], y=requested_params[1],
+                  # hue_order=hue_order,
                   hue="label_desc", palette="muted", legend=True)
     plt.suptitle("Matern Kernel Params for Dog_1 Dataset")
     plt.show()
 
-    # selected interictal parameters
-    X = torch.Tensor(results_df.loc[results_df["label_desc"] == "interictal", requested_params].to_numpy())
+    # selected label_desc parameters
+    X = torch.Tensor(
+        results_df.loc[results_df["label_desc"] == hparams['selected_label_desc'], requested_params].to_numpy())
 
     # standard scale parameters
     X = standard_scale_data(X)
@@ -58,9 +65,14 @@ if __name__ == '__main__':
     clf = mixture.GaussianMixture(n_components=2, covariance_type="full")
     clf.fit(X)
 
+    # persist GMM to disk
+    print("dumping classifier with joblib")
+    dump(clf,
+         f"{config['PATH']['LOCAL']['RESULTS']}/density_estimation_models/GMM_{hparams['selected_label_desc']}_{get_time_as_str()}.joblib")
+
     # display predicted scores by the model as a contour plot
-    x = np.linspace(-20.0, 30.0)
-    y = np.linspace(-20.0, 40.0)
+    x = np.linspace(-10.0, 10.0)
+    y = np.linspace(-10.0, 10.0)
     X, Y = np.meshgrid(x, y)
     XX = np.array([X.ravel(), Y.ravel()]).T
     Z = -clf.score_samples(XX)
@@ -76,9 +88,11 @@ if __name__ == '__main__':
     plt.axis("tight")
     plt.show()
 
+
     def compute_log_likelihood(row):
         XX = np.array(row[requested_params]).reshape(1, -1)
         return clf.score_samples(XX).item()
+
 
     # estimate probability densities of all results
     results_df["log_likelihood"] = results_df.apply(compute_log_likelihood, axis=1)
